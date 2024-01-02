23d1f2120c45bfe8ca13791dbdbc62f7eab3c4ba
sunilkumargupta <62325742+Sunil-731820@users.noreply.github.com>
2024-01-02 23:26:20 +0530

    Build Should failed

--- src/main/java/com/java/dsa/ArrayExample/UseOfForLoop.java
+++ src/main/java/com/java/dsa/ArrayExample/UseOfForLoop.java
@@ -1,8 +1,10 @@
 package com.java.dsa.ArrayExample;
 
-public class FindTheUnionAndIntersectionOfTheTwoSortedArrays {
+public class UseOfForLoop {
 	public static void main(String[] args) {
-		
+		for(i=0;i<100;i++) {
+			System.out.println(i);
+		}
 	}
 
 }
